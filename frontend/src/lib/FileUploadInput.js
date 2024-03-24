bfd5fedd7fbda9b43fd3bb9bcaa71e0803ab6eb3
Deepak <deepak99pmk@gmail.com>
2024-03-24 11:34:25 +0530

    debug in fileupload logic

--- frontend/src/lib/FileUploadInput.js
+++ frontend/src/lib/FileUploadInput.js
@@ -1,11 +1,13 @@
 import { useState, useContext } from "react";
 import { Grid, Button, TextField, LinearProgress } from "@material-ui/core";
 import { CloudUpload } from "@material-ui/icons";
+import CloudUploadIcon from "@material-ui/icons/CloudUpload";
 import Axios from "axios";
 
 import { SetPopupContext } from "../App";
 
 const FileUploadInput = (props) => {
+  // org file in prj
   const setPopup = useContext(SetPopupContext);
 
   const { uploadTo, identifier, handleInput } = props;
@@ -14,12 +16,14 @@ const FileUploadInput = (props) => {
   const [uploadPercentage, setUploadPercentage] = useState(0);
 
   const handleUpload = () => {
-    console.log(file);
+    console.log("incoming file data", file);
+    console.log("uploadTo", uploadTo);
     const data = new FormData();
     data.append("file", file);
     Axios.post(uploadTo, data, {
       headers: {
         "Content-Type": "multipart/form-data",
+        Accept: "application/json, text/plain, /",
       },
       onUploadProgress: (progressEvent) => {
         setUploadPercentage(
@@ -39,14 +43,14 @@ const FileUploadInput = (props) => {
         });
       })
       .catch((err) => {
-        console.log(err.response);
+        console.log("err  ", err);
         setPopup({
           open: true,
           severity: "error",
-          message: err.response.statusText,
-          //   message: err.response.data
-          //     ? err.response.data.message
-          //     : err.response.statusText,
+          message: "Something unexpected happend. Please try again later",
+          // message: err.response.data
+          //   ? err.response.data.message
+          //   : err.response.statusText,
         });
       });
   };
@@ -66,7 +70,7 @@ const FileUploadInput = (props) => {
               type="file"
               style={{ display: "none" }}
               onChange={(event) => {
-                console.log(event.target.files);
+                console.log("file eevent", event.target.files);
                 setUploadPercentage(0);
                 setFile(event.target.files[0]);
               }}
@@ -109,5 +113,197 @@ const FileUploadInput = (props) => {
     </Grid>
   );
 };
+
+// const FileUploadInput_new = (props) => {
+//   // mod file in proj
+//   const setPopup = useContext(SetPopupContext);
+//   const [fileData, setFileData] = useState("");
+//   const [uploadProgress, setUploadProgress] = useState(0);
+//   const { uploadTo, identifier, handleInput } = props;
+
+//   const handleFileUpload = (event) => {
+//     setFileData(event.target.files[0]);
+//     console.log("file", fileData);
+//     const formData = new FormData();
+//     formData.append("file", fileData);
+//     Axios.post(uploadTo, formData, {
+//       headers: {
+//         "Content-Type": "multipart/form-data",
+//       },
+//       onUploadProgress: (progressEvent) => {
+//         const percentCompleted = Math.round(
+//           (progressEvent.loaded * 100) / progressEvent.total
+//         );
+//         setUploadProgress(percentCompleted);
+//       },
+//     })
+//       .then((response) => {
+//         console.log(response);
+//         handleInput(identifier, response.data.url);
+//         setPopup({
+//           open: true,
+//           severity: "success",
+//           message: response.data.message,
+//         });
+//       })
+//       .catch((error) => {
+//         console.error(error);
+//         setPopup({
+//           open: true,
+//           severity: "error",
+//           message: "Something unexpected happend. Please try again later",
+//         });
+//       });
+//   };
+
+//   return (
+//     <Grid container item xs={12} direction="column" className={props.className}>
+//       <Grid container item xs={12} spacing={0}>
+//         <Grid item xs={3}>
+//           <form>
+//             <Button
+//               variant="contained"
+//               color="primary"
+//               component="label"
+//               startIcon={<CloudUploadIcon />}
+//               role={undefined}
+//               tabIndex={-1}
+//             >
+//               Upload
+//               <input
+//                 type="file"
+//                 onChange={handleFileUpload}
+//                 style={{ display: "none" }}
+//               />
+//             </Button>
+//           </form>
+//         </Grid>
+//         <Grid item xs={6}>
+//           <TextField
+//             label={props.label}
+//             value={fileData ? fileData.name || "" : ""}
+//             InputProps={{
+//               readOnly: true,
+//             }}
+//             variant="outlined"
+//             style={{ width: "100%" }}
+//           />
+//         </Grid>
+//         <Grid item xs={3}>
+//           test 3 1
+//         </Grid>
+//       </Grid>
+//       {uploadProgress !== 0 ? (
+//         <Grid item xs={12} style={{ marginTop: "10px" }}>
+//           <LinearProgress variant="determinate" value={uploadProgress} />
+//         </Grid>
+//       ) : null}
+//     </Grid>
+//   );
+// };
+
+// const FileUploadInput_ref = (props) => {
+//   // git file
+//   const setPopup = useContext(SetPopupContext);
+
+//   const { uploadTo, identifier, handleInput } = props;
+
+//   const [file, setFile] = useState("");
+//   const [uploadPercentage, setUploadPercentage] = useState(0);
+
+//   const handleUpload = () => {
+//     console.log(file);
+//     const data = new FormData();
+//     data.append("file", file);
+//     Axios.post(uploadTo, data, {
+//       headers: {
+//         "Content-Type": "multipart/form-data",
+//       },
+//       onUploadProgress: (progressEvent) => {
+//         setUploadPercentage(
+//           parseInt(
+//             Math.round((progressEvent.loaded * 100) / progressEvent.total)
+//           )
+//         );
+//       },
+//     })
+//       .then((response) => {
+//         console.log(response.data);
+//         handleInput(identifier, response.data.url);
+//         setPopup({
+//           open: true,
+//           severity: "success",
+//           message: response.data.message,
+//         });
+//       })
+//       .catch((err) => {
+//         console.error(err);
+//         console.log(err);
+//         setPopup({
+//           open: true,
+//           severity: "error",
+//           message: "Something unexpected happend. Please try again later",
+//         });
+//       });
+//   };
+
+//   return (
+//     <Grid container item xs={12} direction="column" className={props.className}>
+//       <Grid container item xs={12} spacing={0}>
+//         <Grid item xs={3}>
+//           <Button
+//             variant="contained"
+//             color="primary"
+//             component="label"
+//             style={{ width: "100%", height: "100%" }}
+//           >
+//             {props.icon}
+//             <input
+//               type="file"
+//               style={{ display: "none" }}
+//               onChange={(event) => {
+//                 console.log(event.target.files);
+//                 setUploadPercentage(0);
+//                 setFile(event.target.files[0]);
+//               }}
+//               // onChange={onChange}
+//               // onChange={
+//               //   (e) => {}
+//               //   //   setSource({ ...source, place_img: e.target.files[0] })
+//               // }
+//             />
+//           </Button>
+//         </Grid>
+//         <Grid item xs={6}>
+//           <TextField
+//             label={props.label}
+//             value={file ? file.name || "" : ""}
+//             InputProps={{
+//               readOnly: true,
+//             }}
+//             variant="outlined"
+//             style={{ width: "100%" }}
+//           />
+//         </Grid>
+//         <Grid item xs={3}>
+//           <Button
+//             variant="contained"
+//             color="secondary"
+//             style={{ width: "100%", height: "100%" }}
+//             onClick={() => handleUpload()}
+//             disabled={file ? false : true}
+//           >
+//             <CloudUpload />
+//           </Button>
+//         </Grid>
+//       </Grid>
+//       {uploadPercentage !== 0 ? (
+//         <Grid item xs={12} style={{ marginTop: "10px" }}>
+//           <LinearProgress variant="determinate" value={uploadPercentage} />
+//         </Grid>
+//       ) : null}
+//     </Grid>
+//   );
+// };
 
 export default FileUploadInput;
